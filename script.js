a8ccfa00426e106365acdf175d3e48c6dd327671
Vincent Testagrossa <lancevancevt@gmail.com>
2023-11-08 15:54:27 -0500

    made some mistakes with layout. Need to recommit to adding rows within the for loop for every 3 buttons

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const leftContent = document.getElementById('cb-L');
+const rightContent = document.getElementById('cb-R');
 
+// TODO: Need to add rows to the left-content element
+// and change the flex-direction of left-content to 
+// column. Then, each row will have a class that has
+// flex-direction row-reverse and only elements that need
+// to be added per row are added.
 function createNumberBtns(){
+
+    let backBtn = document.createElement('button');
+    let clrBtn = document.createElement('button');
+
+    backBtn.textContent = "<-";
+    clrBtn.textContent = "CE";
+
+    backBtn.classList.add('num-button');
+    clrBtn.classList.add('num-button');
+
+    leftContent.appendChild(backBtn);
+    leftContent.appendChild(clrBtn);
     for (let j = 9; j >= 0; j--){
         let numBtn = document.createElement('button');
         numBtn.textContent = j;
@@ -17,5 +35,35 @@ function createNumberBtns(){
         }
         leftContent.appendChild(numBtn);
     }
+    let divBtn = document.createElement('button');
+    let multBtn = document.createElement('button');
+    let minBtn = document.createElement('button');
+    let plsBtn = document.createElement('button');
+    let eqlsBtn = document.createElement('button');
+
+    divBtn.textContent = "/";
+    multBtn.textContent = "*";
+    minBtn.textContent = "-";
+    plsBtn.textContent = "+";
+    eqlsBtn.textContent = "=";
+
+    divBtn.value = "/";
+    multBtn.value = "*";
+    minBtn.value = "-";
+    plsBtn.value = "+";
+    eqlsBtn.value = "=";
+
+    divBtn.classList.add("operator-button");
+    multBtn.classList.add("operator-button");
+    minBtn.classList.add("operator-button");
+    plsBtn.classList.add("operator-button");
+    eqlsBtn.classList.add("operator-button");
+    
+    rightContent.appendChild(divBtn);
+    rightContent.appendChild(multBtn);
+    rightContent.appendChild(minBtn);
+    rightContent.appendChild(plsBtn);
+    rightContent.appendChild(eqlsBtn);
+
 }
 createNumberBtns();
